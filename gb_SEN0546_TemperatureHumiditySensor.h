dc45011722f7ade038d84b109f5ee6a5aafda218
MrcoSchrnr <MarcoSchreiner@web.de>
2024-03-13 11:43:41 +0100

    added structure of library

---
+++ gb_SEN0546_TemperatureHumiditySensor.h
@@ -0,0 +1,12 @@
+#ifndef gb_SEN0546_TemperatureHumiditySensor.h
+#define gb_SEN0546_TemperatureHumiditySensor.h
+
+#include <Arduino.h>
+
+class gb_SEN0546_TemperatureHumiditySensor
+{
+private:
+public:
+};
+
+#endif
